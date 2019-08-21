145d645046cb0de7606f3aae5000ed20f1cbbcc3
Kevin Quirion <kquirion@iu.edu>
2019-08-21 19:51:43 -0400

    small typo

--- ddxs_Q2_values.py
+++ ddxs_Q2_values.py
@@ -28,13 +28,13 @@ for k in range(len(p_T_1D)):
     cos_mu = p_P/p_mu
     E_mu = sqrt(p_mu**2 +  m_mu**2)
     
-    Q2 = 2.*E_nu*E_mu - 2.*p_mu*cos_mu - m_mu**2
+    Q2 = 2.*E_nu*E_mu - 2.*E_nu*p_mu*cos_mu - m_mu**2
     
 
     g=open("Desktop/Research/Axial FF/txt files/Q2 values/Q2_vals_pt=%s.txt" % p_T_1D[k],"w+")
     g.write("     Q2            E_nu            p_P             \n\n")
     for i in range(len(p_P_1D)):
-        for j in range(len(p_T_1D)):
+        for j in range(len(E_nu_flux)):
             g.write("    %s            %s            %s \n" % (Q2[i,j],E_nu[i,j],p_P[i,j]))
     g.close()    
     
